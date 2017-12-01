a4b81cb606458cb284cd4aef3e73b9a65026d95d
Pivotal <pivotal@pivotal.io>
2017-12-01 11:03:07 -0800

    parameterize the script

--- tasks/create-edge/task.sh
+++ tasks/create-edge/task.sh
@@ -26,40 +26,70 @@ pynsxv_local() {
   python pynsxv/cli.py "$@"
 }
 
-LABWIRES="labwire-proto-01 labwire-proto-02"
+get_cidr() {
+  IP=$1
+  MASK=$2
+  FIRST_THREE=$(echo $IP|cut -d. -f 1,2,3)
+  echo "$FIRST_THREE.0/$MASK"
+}
 
-
-for labwire in $LABWIRES; do
-  pynsxv_local lswitch -n $labwire create
+NUM_LOGICAL_SWITCHES=3
+# Create logical switches
+for labwire_id in $(seq $NUM_LOGICAL_SWITCHES); do
+  pynsxv_local lswitch -n "labwire-proto-0$labwire_id" create
 done
 
+# Create an edge
 pynsxv_local esg create -n $NSX_EDGE_GEN_NAME -pg "$ESG_DEFAULT_UPLINK_PG_1"
 
-pynsxv_local esg cfg_interface -n $NSX_EDGE_GEN_NAME --portgroup "$ESG_DEFAULT_UPLINK_PG_1" \
+# Connect the edge to a backbone
+pynsxv_local esg cfg_interface \
+  -n $NSX_EDGE_GEN_NAME \
+  --portgroup "$ESG_DEFAULT_UPLINK_PG_1" \
   --vnic_index 0 --vnic_type uplink --vnic_name "External" \
-  --vnic_ip 10.193.99.170 --vnic_mask 255.255.255.0
+  --vnic_ip $ESG_DEFAULT_UPLINK_IP_1 --vnic_mask 255.255.255.0
 
-pynsxv_local esg cfg_interface -n $NSX_EDGE_GEN_NAME --logical_switch my-test-switch \
-  --vnic_index 1 --vnic_type internal --vnic_name transit \
-  --vnic_ip 192.168.10.1 --vnic_mask 255.255.255.0
+# Attach logical switches to an edge
+for labwire_id in $(seq $NUM_LOGICAL_SWITCHES); do
+  pynsxv_local esg cfg_interface -n $NSX_EDGE_GEN_NAME --logical_switch "labwire-proto-0$labwire_id" \
+    --vnic_index 1 --vnic_type internal --vnic_name Transit \
+    --vnic_ip "192.168.1$labwire_id.1" --vnic_mask 255.255.255.0
+done
 
-# Configure firewall + default gw + static route
+# Configure firewall
 pynsxv_local esg set_fw_status -n $NSX_EDGE_GEN_NAME --fw_default accept
 
-pynsxv_local esg set_dgw -n $NSX_EDGE_GEN_NAME --next_hop "$ESG_DEFAULT_UPLINK_PG_1"
+# Set default gateway
+pynsxv_local esg set_dgw \
+  -n $NSX_EDGE_GEN_NAME \
+  --next_hop "$ESG_DEFAULT_UPLINK_IP_1"
 
-pynsxv_local esg add_route -n $NSX_EDGE_GEN_NAME --route_net 10.1.0.0/22 --next_hop 172.16.1.2
+# Set static route
+pynsxv_local esg add_route \
+  -n $NSX_EDGE_GEN_NAME \
+  --route_net $(get_cidr $ESG_GATEWAY_1 24) \
+  --next_hop "$ESG_GATEWAY_1"
 
 # Enable load balancing
 pynsxv_local lb enable_lb -n $NSX_EDGE_GEN_NAME
-# Create lb app profile
 
-pynsxv_local lb add_profile -n $NSX_EDGE_GEN_NAME --profile_name appprofile --protocol HTTP
+# Create lb app profile
+pynsxv_local lb add_profile \
+  -n $NSX_EDGE_GEN_NAME \
+  --profile_name appprofile \
+  --protocol HTTP
 
 # create lb pool
-pynsxv_local lb add_pool -n $NSX_EDGE_GEN_NAME --pool_name pool_web --transparent true
+pynsxv_local lb add_pool -n $NSX_EDGE_GEN_NAME \
+  --pool_name pool_web \
+  --transparent true
 
 # create lb vip
-pynsxv_local esg cfg_interface -n $NSX_EDGE_GEN_NAME --vnic_index 0 --vnic_ip 20.20.20.2 --vnic_mask 255.255.255.0 --vnic_secondary_ips 20.20.20.6
-
-pynsxv_local lb add_vip -n $NSX_EDGE_GEN_NAME --vip_name vip_app --pool_name pool_app --profile_name appprofile --vip_ip 172.16.1.6 --protocol HTTP --port 80
+pynsxv_local lb add_vip \
+  -n $NSX_EDGE_GEN_NAME \
+  --vip_name vip-gorouter-http-1 \
+  --pool_name pool_app \
+  --profile_name appprofile \
+  --vip_ip $ESG_GO_ROUTER_UPLINK_IP_1  \
+  --protocol HTTP \
+  --port 80
