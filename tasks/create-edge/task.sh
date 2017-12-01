84688197fc1058ee95bfead5220b5c1cb0fdc143
Slawek Ligus <sligus@pivotal.io>
2017-11-30 16:17:01 -0800

    Create a switch and an edge
    [#152981176]

--- tasks/create-edge/task.sh
+++ tasks/create-edge/task.sh
@@ -1,3 +1,26 @@
 #!/bin/bash -eux
 
 echo "Creating edge"
+
+cat << EOF > pynsxv/nsx.ini
+[nsxv]
+nsx_manager = $NSX_EDGE_GEN_NSX_MANAGER_ADDRESS
+nsx_username = $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_USER
+nsx_password = $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_PASSWD
+
+[vcenter]
+vcenter = $VCENTER_HOST
+vcenter_user = $VCENTER_USR
+vcenter_passwd = $VCENTER_PWD
+
+[defaults]
+transport_zone = $NSX_EDGE_GEN_NSX_MANAGER_TRANSPORT_ZONE
+datacenter_name = $VCENTER_DATA_CENTER
+edge_datastore =  $NSX_EDGE_GEN_EDGE_DATASTORE
+edge_cluster = $NSX_EDGE_GEN_EDGE_CLUSTER
+EOF
+
+pushd pynsxv
+
+python pynsxv/cli.py lswitch -n my-test-switch create
+python pynsxv/cli.py esg create -n my_test_esg -pg "$ESG_DEFAULT_UPLINK_PG_1"
